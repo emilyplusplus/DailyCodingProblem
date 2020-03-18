5e6a1d6833115ad75e1f8664ea7c40735a59a969
Emily Carlsen <emilytcarlsen@gmail.com>
2020-03-18 13:25:59 -0400

    Tweak #23 and add debug launch config for debugging longer scripts

--- 23-wall-matrix-navigate-min-steps.js
+++ 23-wall-matrix-navigate-min-steps.js
@@ -15,154 +15,143 @@ For example, given the following board:
 and start = (3, 0) (bottom left) and end = (0, 0) (top left), the minimum number of steps required to reach the end is 7, since we would need to go through (1, 2) because there is a wall everywhere else on the second row.
 */
 
+//use breadth-first path finding algorithm, code from Greg Trowbridge, with minor modifications to address #23 (i.e. get length specifically and match board they gave)
 //http://gregtrowbridge.com/a-basic-pathfinding-algorithm/
 
-// Start location will be in the following format:
-// [distanceFromTop, distanceFromLeft]
 var findShortestPath = function(startCoordinates, grid) {
-    var distanceFromTop = startCoordinates[0];
-    var distanceFromLeft = startCoordinates[1];
-  
-    // Each "location" will store its coordinates
-    // and the shortest path required to arrive there
-    var location = {
-      distanceFromTop: distanceFromTop,
-      distanceFromLeft: distanceFromLeft,
-      path: [],
-      status: 'Start'
-    };
-  
-    // Initialize the queue with the start location already inside
-    var queue = [location];
-  
-    // Loop through the grid searching for the goal
-    while (queue.length > 0) {
-      // Take the first location off the queue
-      var currentLocation = queue.shift();
-  
-      // Explore North
-      var newLocation = exploreInDirection(currentLocation, 'North', grid);
-      if (newLocation.status === 'Goal') {
-        return newLocation.path;
-      } else if (newLocation.status === 'Valid') {
-        queue.push(newLocation);
-      }
-  
-      // Explore East
-      var newLocation = exploreInDirection(currentLocation, 'East', grid);
-      if (newLocation.status === 'Goal') {
-        return newLocation.path;
-      } else if (newLocation.status === 'Valid') {
-        queue.push(newLocation);
-      }
-  
-      // Explore South
-      var newLocation = exploreInDirection(currentLocation, 'South', grid);
-      if (newLocation.status === 'Goal') {
-        return newLocation.path;
-      } else if (newLocation.status === 'Valid') {
-        queue.push(newLocation);
-      }
-  
-      // Explore West
-      var newLocation = exploreInDirection(currentLocation, 'West', grid);
-      if (newLocation.status === 'Goal') {
-        return newLocation.path;
-      } else if (newLocation.status === 'Valid') {
-        queue.push(newLocation);
-      }
-    }
-  
-    // No valid path found
-    return false;
-  
+  var distanceFromTop = startCoordinates[0];
+  var distanceFromLeft = startCoordinates[1];
+
+  // Each "location" will store its coordinates
+  // and the shortest path required to arrive there
+  var location = {
+    distanceFromTop: distanceFromTop,
+    distanceFromLeft: distanceFromLeft,
+    path: [],
+    status: 'Start'
   };
-  
-  // This function will check a location's status
-  // (a location is "valid" if it is on the grid, is not an "obstacle",
-  // and has not yet been visited by our algorithm)
-  // Returns "Valid", "Invalid", "Blocked", or "Goal"
-  var locationStatus = function(location, grid) {
-    var gridSize = grid.length;
-    var dft = location.distanceFromTop;
-    var dfl = location.distanceFromLeft;
-  
-    if (location.distanceFromLeft < 0 ||
-        location.distanceFromLeft >= gridSize ||
-        location.distanceFromTop < 0 ||
-        location.distanceFromTop >= gridSize) {
-  
-      // location is not on the grid--return false
-      return 'Invalid';
-    } else if (grid[dft][dfl] === 'Goal') {
-      return 'Goal';
-    } else if (grid[dft][dfl] !== 'Empty') {
-      // location is either an obstacle or has been visited
-      return 'Blocked';
-    } else {
-      return 'Valid';
+
+  // Initialize the queue with the start location already inside
+  var queue = [location];
+
+  // Loop through the grid searching for the goal
+  while (queue.length > 0) {
+    // Take the first location off the queue
+    var currentLocation = queue.shift();
+
+    // Explore North
+    var newLocation = exploreInDirection(currentLocation, 'North', grid);
+    if (newLocation.status === 'Goal') {
+      return newLocation.path;
+    } else if (newLocation.status === 'Valid') {
+      queue.push(newLocation);
     }
-  };
-  
-  
-  // Explores the grid from the given location in the given
-  // direction
-  var exploreInDirection = function(currentLocation, direction, grid) {
-    var newPath = currentLocation.path.slice();
-    newPath.push(direction);
-  
-    var dft = currentLocation.distanceFromTop;
-    var dfl = currentLocation.distanceFromLeft;
-  
-    if (direction === 'North') {
-      dft -= 1;
-    } else if (direction === 'East') {
-      dfl += 1;
-    } else if (direction === 'South') {
-      dft += 1;
-    } else if (direction === 'West') {
-      dfl -= 1;
+
+    // Explore East
+    var newLocation = exploreInDirection(currentLocation, 'East', grid);
+    if (newLocation.status === 'Goal') {
+      return newLocation.path;
+    } else if (newLocation.status === 'Valid') {
+      queue.push(newLocation);
     }
-  
-    var newLocation = {
-      distanceFromTop: dft,
-      distanceFromLeft: dfl,
-      path: newPath,
-      status: 'Unknown'
-    };
-    newLocation.status = locationStatus(newLocation, grid);
-  
-    // If this new location is valid, mark it as 'Visited'
-    if (newLocation.status === 'Valid') {
-      grid[newLocation.distanceFromTop][newLocation.distanceFromLeft] = 'Visited';
+
+    // Explore South
+    var newLocation = exploreInDirection(currentLocation, 'South', grid);
+    if (newLocation.status === 'Goal') {
+      return newLocation.path;
+    } else if (newLocation.status === 'Valid') {
+      queue.push(newLocation);
     }
-  
-    return newLocation;
-  };
-  
-  
-  // OK. We have the functions we need--let's run them to get our shortest path!
-  
-  // Create a 4x4 grid
-  // Represent the grid as a 2-dimensional array
-  var gridSize = 4;
-  var grid = [];
-  for (var i=0; i<gridSize; i++) {
-    grid[i] = [];
-    for (var j=0; j<gridSize; j++) {
-      grid[i][j] = 'Empty';
+
+    // Explore West
+    var newLocation = exploreInDirection(currentLocation, 'West', grid);
+    if (newLocation.status === 'Goal') {
+      return newLocation.path;
+    } else if (newLocation.status === 'Valid') {
+      queue.push(newLocation);
     }
   }
-  
-  // Think of the first index as "distance from the top row"
-  // Think of the second index as "distance from the left-most column"
-  
-  // This is how we would represent the grid with obstacles above
-  grid[3][0] = "Start";
-  grid[0][0] = "Goal";
-  
-  grid[1][0] = "Obstacle";
-  grid[1][1] = "Obstacle";
-  grid[1][3] = "Obstacle";
-  
-  console.log(findShortestPath([3,0], grid).length);
+
+  // No valid path found
+  return false;
+
+};
+
+// This function will check a location's status
+// (a location is "valid" if it is on the grid, is not an "obstacle",
+// and has not yet been visited by our algorithm)
+// Returns "Valid", "Invalid", "Blocked", or "Goal"
+var locationStatus = function(location, grid) {
+  var gridSize = grid.length;
+  var dft = location.distanceFromTop;
+  var dfl = location.distanceFromLeft;
+
+  if (location.distanceFromLeft < 0 ||
+      location.distanceFromLeft >= gridSize ||
+      location.distanceFromTop < 0 ||
+      location.distanceFromTop >= gridSize) {
+
+    // location is not on the grid--return false
+    return 'Invalid';
+  } else if (grid[dft][dfl] === 'Goal') {
+    return 'Goal';
+  } else if (grid[dft][dfl] !== 'Empty') {
+    // location is either an obstacle or has been visited
+    return 'Blocked';
+  } else {
+    return 'Valid';
+  }
+};
+
+var exploreInDirection = function(currentLocation, direction, grid) {
+  var newPath = currentLocation.path.slice();
+  newPath.push(direction);
+
+  var dft = currentLocation.distanceFromTop;
+  var dfl = currentLocation.distanceFromLeft;
+
+  if (direction === 'North') {
+    dft -= 1;
+  } else if (direction === 'East') {
+    dfl += 1;
+  } else if (direction === 'South') {
+    dft += 1;
+  } else if (direction === 'West') {
+    dfl -= 1;
+  }
+
+  var newLocation = {
+    distanceFromTop: dft,
+    distanceFromLeft: dfl,
+    path: newPath,
+    status: 'Unknown'
+  };
+  newLocation.status = locationStatus(newLocation, grid);
+
+  // If this new location is valid, mark it as 'Visited'
+  if (newLocation.status === 'Valid') {
+    grid[newLocation.distanceFromTop][newLocation.distanceFromLeft] = 'Visited';
+  }
+
+  return newLocation;
+};
+
+var gridSize = 4;
+var grid = [];
+for (var i=0; i<gridSize; i++) {
+  grid[i] = [];
+  for (var j=0; j<gridSize; j++) {
+    grid[i][j] = 'Empty';
+  }
+}
+
+grid[3][0] = "Start";
+grid[0][0] = "Goal";
+
+grid[1][0] = "Obstacle";
+grid[1][1] = "Obstacle";
+grid[1][3] = "Obstacle";
+
+let shortestPath = findShortestPath([3,0], grid)
+
+console.log(shortestPath.length);
