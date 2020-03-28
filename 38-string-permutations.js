5edb680c2af5c6378cde5b552a0824f3d6c82b08
Emily Carlsen <emilytcyt@gmail.com>
2020-03-27 20:26:27 -0400

    Create 38-string-permutations.js

---
+++ 38-string-permutations.js
@@ -0,0 +1,28 @@
+function permutations(str) {
+  let letters = str.split('')
+  let results = [[letters.shift()]]
+
+  while (letters.length) {
+      const currLetter = letters.shift()
+      let tmpResults = []
+      results.forEach(result => {
+          let rIdx = 0
+          while (rIdx <= result.length) {
+              const tmp = [...result]
+              tmp.splice(rIdx, 0, currLetter)
+              tmpResults.push(tmp)
+              rIdx++
+          }
+      })
+      results = tmpResults
+  }
+
+  return results
+    .map(letterArray => letterArray.join(''))
+    .filter((el, idx, self) => (self.indexOf(el) === idx))
+    .sort()
+}
+
+console.log( permutations('a') ) //['a']
+console.log( permutations('ab') ) //['ab', 'ba']
+console.log( permutations('aabb') ) //['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
