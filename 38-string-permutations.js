059506f98f551e08fddb559da5fd1181833fb5b5
Emily Carlsen <emilytcyt@gmail.com>
2020-03-27 20:38:32 -0400

    Update 38-string-permutations.js

--- 38-string-permutations.js
+++ 38-string-permutations.js
@@ -1,3 +1,11 @@
+/*
+Daily Coding Problem #38
+
+In this kata you have to create all permutations of an input string and
+remove duplicates, if present. This means, you have to shuffle all
+letters from the input in all possible orders.
+*/
+
 function permutations(str) {
   let letters = str.split('')
   let results = [[letters.shift()]]
